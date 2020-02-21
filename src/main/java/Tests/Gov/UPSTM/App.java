4716e7c60e06dc13a4cf64ac16d4a4fa6a54a982
Nillab Waez <you@example.com>
2020-02-21 14:57:20 -0500

    first commit

---
+++ src/main/java/Tests/Gov/UPSTM/App.java
@@ -0,0 +1,13 @@
+package Tests.Gov.UPSTM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
